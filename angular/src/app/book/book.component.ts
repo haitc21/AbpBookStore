ceba5b8918932cb4b3e89f18824cf9008fb110e4
haitc <hai.tc21@gmail.com>
2022-12-22 22:58:00 +0700

    part10

--- angular/src/app/book/book.component.ts
+++ angular/src/app/book/book.component.ts
@@ -1,9 +1,11 @@
 import { ListService, PagedResultDto } from '@abp/ng.core';
 import { Component, OnInit } from '@angular/core';
-import { BookService, BookDto, bookTypeOptions } from '@proxy/books';
+import { BookService, BookDto, bookTypeOptions, AuthorLookupDto } from '@proxy/books';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NgbDateNativeAdapter, NgbDateAdapter } from '@ng-bootstrap/ng-bootstrap';
 import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-book',
@@ -14,9 +16,11 @@ import { ConfirmationService, Confirmation } from '@abp/ng.theme.shared';
 export class BookComponent implements OnInit {
   book = { items: [], totalCount: 0 } as PagedResultDto<BookDto>;
 
-  selectedBook = {} as BookDto; // declare selectedBook
-
   form: FormGroup;
+
+  selectedBook = {} as BookDto;
+
+  authors$: Observable<AuthorLookupDto[]>;
 
   bookTypes = bookTypeOptions;
 
@@ -26,29 +30,27 @@ export class BookComponent implements OnInit {
     public readonly list: ListService,
     private bookService: BookService,
     private fb: FormBuilder,
-    private confirmation: ConfirmationService // inject the ConfirmationService
-  ) {}
+    private confirmation: ConfirmationService
+  ) {
+    this.authors$ = bookService.getAuthorLookup().pipe(map((r) => r.items));
+  }
 
   ngOnInit() {
-    const bookStreamCreator = query => this.bookService.getList(query);
+    const bookStreamCreator = (query) => this.bookService.getList(query);
 
-    this.list.hookToQuery(bookStreamCreator).subscribe(response => {
+    this.list.hookToQuery(bookStreamCreator).subscribe((response) => {
       this.book = response;
     });
   }
-  getKeyBookType(type) {
-    return this.bookTypes.find(x => x.value == type).key;
-  }
 
   createBook() {
-    this.selectedBook = {} as BookDto; // reset the selected book
+    this.selectedBook = {} as BookDto;
     this.buildForm();
     this.isModalOpen = true;
   }
 
-  // Add editBook method
   editBook(id: string) {
-    this.bookService.get(id).subscribe(book => {
+    this.bookService.get(id).subscribe((book) => {
       this.selectedBook = book;
       this.buildForm();
       this.isModalOpen = true;
@@ -57,7 +59,8 @@ export class BookComponent implements OnInit {
 
   buildForm() {
     this.form = this.fb.group({
-      name: [this.selectedBook.name || '', Validators.required],
+      authorId: [this.selectedBook.authorId || null, Validators.required],
+      name: [this.selectedBook.name || null, Validators.required],
       type: [this.selectedBook.type || null, Validators.required],
       publishDate: [
         this.selectedBook.publishDate ? new Date(this.selectedBook.publishDate) : null,
@@ -67,7 +70,6 @@ export class BookComponent implements OnInit {
     });
   }
 
-  // change the save method
   save() {
     if (this.form.invalid) {
       return;
@@ -85,10 +87,13 @@ export class BookComponent implements OnInit {
   }
 
   delete(id: string) {
-    this.confirmation.warn('::AreYouSureToDelete', '::AreYouSure').subscribe(status => {
+    this.confirmation.warn('::AreYouSureToDelete', 'AbpAccount::AreYouSure').subscribe((status) => {
       if (status === Confirmation.Status.confirm) {
         this.bookService.delete(id).subscribe(() => this.list.get());
       }
     });
+  }
+  getKeyBookType(type) {
+    return this.bookTypes.find((t) => t.value === type).key;
   }
 }
